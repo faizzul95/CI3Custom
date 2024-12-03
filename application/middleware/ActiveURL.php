35ffe2ae6671fb4c0e2c7bac240baefd1fd8d20a
Fahmy Izwan <mfahmyizwan@gmail.com>
2024-12-03 18:01:57 +0800

    Create ActiveURL.php

---
+++ application/middleware/ActiveURL.php
@@ -0,0 +1,18 @@
+<?php
+
+# application/middleware/ActiveURL.php
+
+class ActiveURL implements Luthier\MiddlewareInterface
+{
+    public function run($args)
+    {
+        $ci = &get_instance();
+        model('SystemMenuNavigation_model');
+
+        $isActive = $ci->SystemMenuNavigation_model->where('menu_url', uri_string())->where('is_active', '1')->count();
+
+        if (!$isActive) {
+            show_404();
+        }
+    }
+}
